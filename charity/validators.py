5b0c6c29ed4a610beacaee0f30c840ccf9719c35
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-06-15 22:34:22 +0200

    usunięcie duplikacji w walidatorach

--- charity/validators.py
+++ charity/validators.py
@@ -3,26 +3,27 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 from django.contrib.auth import authenticate
 
-class SimilarOldPasswordValidator(object):
 
+class SimilarOldPasswordValidator(object):
     def validate(self, password, user=None):
-        if authenticate(username = user.username, password=password):
-            raise ValidationError(_('nowe hasło takie jak stare'), code ='to samo hasło')
+        if authenticate(username=user.username, password=password):
+            raise ValidationError(_(self.get_help_text()), code='the_same_password')
 
     def get_help_text(self):
         return 'Nowe hasło musi byc różne od starego'
+
 
 class NumberValidator(object):
     def validate(self, password, user=None):
         if not re.findall('\d', password):
             raise ValidationError(
-                _("Hasło musi zawierać co najmniej 1 cyfrę, 0-9."),
+                _(self.get_help_text()),
                 code='password_no_number',
             )
 
     def get_help_text(self):
         return _(
-            "Hasło musi zawierać co najmniej 1 cyfrę,, 0-9."
+            "Hasło musi zawierać co najmniej 1 cyfrę, 0-9."
         )
 
 
@@ -30,7 +31,7 @@ class UppercaseValidator(object):
     def validate(self, password, user=None):
         if not re.findall('[A-Z]', password):
             raise ValidationError(
-                _("Hasło musi zawierać co najmniej 1 wielką literę, A-Z."),
+                _(self.get_help_text()),
                 code='password_no_upper',
             )
 
@@ -44,7 +45,7 @@ class LowercaseValidator(object):
     def validate(self, password, user=None):
         if not re.findall('[a-z]', password):
             raise ValidationError(
-                _("Hasło musi zawierać co najmniej 1 małą literę, a-z."),
+                _(self.get_help_text()),
                 code='password_no_lower',
             )
 
@@ -58,8 +59,7 @@ class SymbolValidator(object):
     def validate(self, password, user=None):
         if not re.findall('[()[\]{}|\\`~!@#$%^&*_\-+=;:\'",<>./?]', password):
             raise ValidationError(
-                _("Hasło musi zawierać co najmniej 1 symbol: " +
-                  "()[]{}|\`~!@#$%^&*_-+=;:'\",<>./?"),
+                _(self.get_help_text()),
                 code='password_no_symbol',
             )
 
