89379e425d3f4b06ca3a9052c65bbca46ff7d004
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-06-18 23:04:17 +0200

    user as deleted when deleted

--- charity/models.py
+++ charity/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 
 def get_sentinel_user():
-    return get_user_model().objects.get_or_create(username='deleted')[0]
+    return get_user_model().objects.get_or_create(username='deleted', is_active=False)[0]
 
 
 def get_sentinel_institution():
