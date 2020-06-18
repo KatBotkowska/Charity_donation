7cdf4da65062ffbad9eab7fd199f5421581aff6d
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-06-18 23:04:17 +0200

    start tests for models

---
+++ charity/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from charity.models import Category, Institution, Donation, InstitutionChoices
+
+
+class CategoryModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        category = Category.objects.create(name='test_category')
+        return category
+
+
+    def test_get_object(self):
+        test_category = Category.objects.all()
+        self.assertEquals(len(test_category), 1)
+        self.assertEquals('test_category', test_category[0].name)
+        #self.assertEquals(Category.objects.get(name='test_category').name, self.category().name)
+        self.assertEquals(Category.objects.get(name='test_category').name, self.setUpTestData().name)
+
+
+    def test_name_label(self):
+        category = Category.objects.get(id=1)
+        field_label = category._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+
+    def test_name_max_length(self):
+        category = Category.objects.get(id=1)
+        max_length = category._meta.get_field('name').max_length
+        self.assertEquals(max_length, 256)
+
+    def test_object_str_name(self):
+        category = Category.objects.get(id=1)
+        expected_object_name = f'{category.name}'
+        self.assertEquals(expected_object_name, str(category))
+
+class IstitutionModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        test_category = Category.objects.create(name='test_category')
+        Institution.objects.create(name='test_institution', description='institution for test purpose', categories=test_category )
