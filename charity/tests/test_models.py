46fc486a71e4cfb7cdb2f323345fdf15abd918b7
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-06-19 23:16:06 +0200

    tests for category get object

--- charity/tests/test_models.py
+++ charity/tests/test_models.py
@@ -6,16 +6,12 @@ class CategoryModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         category = Category.objects.create(name='test_category')
-        return category
-
 
     def test_get_object(self):
         test_category = Category.objects.all()
         self.assertEquals(len(test_category), 1)
         self.assertEquals('test_category', test_category[0].name)
-        #self.assertEquals(Category.objects.get(name='test_category').name, self.category().name)
-        self.assertEquals(Category.objects.get(name='test_category').name, self.setUpTestData().name)
-
+        self.assertEquals(Category.objects.filter(name='test_category').count(), 1)
 
     def test_name_label(self):
         category = Category.objects.get(id=1)
@@ -32,8 +28,10 @@ class CategoryModelTest(TestCase):
         expected_object_name = f'{category.name}'
         self.assertEquals(expected_object_name, str(category))
 
+
 class IstitutionModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         test_category = Category.objects.create(name='test_category')
-        Institution.objects.create(name='test_institution', description='institution for test purpose', categories=test_category )
+        Institution.objects.create(name='test_institution', description='institution for test purpose',
+                                   categories=test_category)
