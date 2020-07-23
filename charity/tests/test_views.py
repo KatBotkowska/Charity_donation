4ad0e4e3596fbbfb0ab8211aacbac8368db02225
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-07-23 23:19:03 +0200

    test view logout i start register

--- charity/tests/test_views.py
+++ charity/tests/test_views.py
@@ -157,3 +157,31 @@ class LoginViewTest(TestCase):
         response = self.client.get(reverse('charity:login'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'login.html')
+
+class LogoutViewTest(TestCase):
+    def test_url_exists_at_desired_location(self):
+        response = self.client.get('/logout')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        response = self.client.get(reverse('charity:logout'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('charity:logout'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+class RegisterViewTest(TestCase):
+    def test_url_exists_at_desired_location(self):
+        response = self.client.get('/register')
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_accessible_by_name(self):
+        response = self.client.get(reverse('charity:register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('charity:register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')
