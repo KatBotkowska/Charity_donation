122f94a00a8d25e5f1089d13f83f4f66cffe95f2
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-09-06 22:57:59 +0200

    test for patch update

--- charity/tests/test_API.py
+++ charity/tests/test_API.py
@@ -105,7 +105,7 @@ class CreateNewUserTest(TestCase):
         response = self.client.post(reverse('user-list'), data = json.dumps(self.valid_payload), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-class UpdateUserTest(TestCase):
+class UpdateUserPutTest(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user(first_name='user1', last_name='user_1', username='test_username1',
                                              email='user1@email.com',
@@ -138,3 +138,35 @@ class UpdateUserTest(TestCase):
                                    content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+class UpdateUserPatchTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(first_name='user1', last_name='user_1', username='test_username1',
+                                             email='user1@email.com',
+                                             password='1Top_secret@1')
+        self.user2 = User.objects.create_user(first_name='user2', last_name='user_2', username='test_username2',
+                                             email='user2@email.com',
+                                             password='2Top_secret@1')
+        self.valid_payload = {'imie':'user2_update', 'nazwisko':'user_2_update',
+                                             'email':'user2@email.com',
+                                             'haslo':'2Top_secret@1'}
+        self.invalid_payload = {'imie':'user2', 'nazwisko':'user_2',
+                                             'email':'user2email.com',
+                                             'haslo':'aa'}
+        self.client = APIClient()
+
+    def test_update_user_status_code_if_authenticated(self):
+        self.client.login(username='test_username1', password='1Top_secret@1')
+        response = self.client.patch(reverse('user-detail', args =[self.user2.pk]), data = json.dumps(self.valid_payload),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_user_status_code_if_not_authenticated(self):
+        response = self.client.patch(reverse('user-detail', args =[self.user2.pk]), data = json.dumps(self.valid_payload),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_user_status_code_if_authenticated_not_valid_data(self):
+        self.client.login(username='test_username1', password='1Top_secret@1')
+        response = self.client.patch(reverse('user-detail', args =[self.user2.pk]), data = json.dumps(self.invalid_payload),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
