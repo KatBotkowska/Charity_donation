eca764d12751c235c7281441e6e598f9ffa8ad2b
Kasia Botkowska <katarzyna.botkowska@gmail.com>
2020-09-10 22:17:49 +0200

    put update

--- charity/tests/test_API.py
+++ charity/tests/test_API.py
@@ -395,6 +395,7 @@ class InstitutionViewSetTest(TestCase):
         serializer = InstitutionSerializer(institutions, many=True, context=serializer_context)
         self.assertEqual(json.loads(response.content)['results'][0], serializer.data[0])
 
+
 class GetSingleInstitutionTest(TestCase):
     def setUp(self):
         self.test_user = User.objects.create_user(first_name='user', last_name='user', username='test_username',
@@ -427,3 +428,37 @@ class GetSingleInstitutionTest(TestCase):
         self.client.login(username='test_username', password='Top_secret@1')
         response = self.client.get(reverse('institution-detail', args=[23]), format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+class UpdateInstitutionPutTest(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(first_name='user', last_name='user', username='test_username',
+                                                  email='user@email.com',
+                                                  password='Top_secret@1')
+        self.test_category_zabawki = Category.objects.create(name='zabawki')
+        self.test_category_meble = Category.objects.create(name='meble')
+        self.test_institution = Institution.objects.create(name='test_institution',
+                                                           description='institution for test purpose')
+        self.test_institution.categories.add(self.test_category_zabawki, self.test_category_meble)
+        self.valid_payload = {'name': 'changed_institution', 'description':'changed institution for test purpose', 'categories':[self.test_category_zabawki.pk]}
+        self.invalid_payload = {'name': ''}
+        self.client = APIClient()
+
+    def test_update_institution_status_code_if_authenticated(self):
+        self.client.login(username='test_username', password='Top_secret@1')
+        response = self.client.put(reverse('institution-detail', args=[self.test_institution.pk]),
+                                   data=json.dumps(self.valid_payload),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_institution_status_code_if_not_authenticated(self):
+        response = self.client.put(reverse('institution-detail', args=[self.test_institution.pk]),
+                                   data=json.dumps(self.valid_payload),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_category_status_code_if_authenticated_not_valid_data(self):
+        self.client.login(username='test_username', password='Top_secret@1')
+        response = self.client.put(reverse('institution-detail', args=[self.test_institution.pk]),
+                                   data=json.dumps(self.invalid_payload),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
